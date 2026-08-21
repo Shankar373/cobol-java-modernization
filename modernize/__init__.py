2c86b1f74f8fe64481fc7d18f1c095d92402caf0
Shankar373 <bandishankar373@gmail.com>
2026-08-21 18:19:33 +0530

    Implement and verify Phase 3.5 generic CALL and dependency analysis engine with tests and report

--- modernize/__init__.py
+++ modernize/__init__.py
@@ -1,7 +1,7 @@
 from .semantic_ir import SemanticIR, SemanticIRNode
 from .control_flow import ControlFlowModel, ControlFlowEdge, CFGNode, CFGEdge
 from .data_flow import DataFlowModel, DataFlowTransition, DFNode, DFEdge
-from .dependencies import DependencyMigrationStatus, CallDependencyRecord
+from .dependencies import DependencyMigrationStatus, CallDependencyRecord, DependencyAnalysisEngine
 from .traceability import TraceabilityModel, TraceabilityRecord
 from .coverage import BusinessRuleCoverage
 from .lexer import CobolLexer, CobolToken
@@ -20,6 +20,7 @@ __all__ = [
     "DFEdge",
     "DependencyMigrationStatus",
     "CallDependencyRecord",
+    "DependencyAnalysisEngine",
     "TraceabilityModel",
     "TraceabilityRecord",
     "BusinessRuleCoverage",
